ff0d8ef04599598dbe35979fe71740e6d79c5272
Kirson Greer <greer405@stcharles.k12.la.us>
2023-11-07 21:08:53 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,28 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+    var circle; 
+    var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        SVGFESpecularLightingElement
+        circles.push(circle);
+        }
+
+
+            // Code to draw a circle
+
+
+            
+    
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        drawCircle();
+
 
 
         ////////////////////////////////////////////////////////////
